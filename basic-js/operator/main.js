09a46c90ce14d698f1799350351c7297e23e970e
EUNKYUNG JUNG <thxjek@gmail.com>
2022-01-17 15:37:28 +0900

    Add variable-02

--- basic-js/operator/main.js
+++ basic-js/operator/main.js
@@ -1,12 +1,12 @@
-function fact(n) {
-    if (n <= 1) return n;
-    return n * fact(n - 1);
-}
+// function fact(n) {
+//     if (n <= 1) return n;
+//     return n * fact(n - 1);
+// }
 
-for (var i = 1; i < 10; i++) {
-    document.write(i + "i=" + fact(i) + "<br/>");
-}
+// for (var i = 1; i < 10; i++) {
+//     document.write(i + "i=" + fact(i) + "<br/>");
+// }
 
-var obj = {};
-obj[Symbol("heart")] = 3;
-console.log(obj);
+// var obj = {};
+// obj[Symbol("heart")] = 3;
+// console.log(obj);
