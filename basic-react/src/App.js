96d5f189aa2759911a2d7e9f04bfdb054ae89cc4
luckyjek <70943889+luckyjek@users.noreply.github.com>
2022-03-04 10:31:27 +0900

    study simple props

---
+++ basic-react/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Hello from './components/hello';
+
+function App() {
+  return(
+    <div>
+      <Hello name="react" color="red"/>
+      <Hello color="pink"/>
+    </div>
+
+
+)
+
+}
+
+export default App;
+
