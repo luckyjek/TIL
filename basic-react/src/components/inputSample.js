76b74e5329be6130014b62713880d54b244e1fdc
luckyjek <70943889+luckyjek@users.noreply.github.com>
2022-03-04 15:19:09 +0900

    about multiple inputs

--- basic-react/src/components/inputSample.js
+++ basic-react/src/components/inputSample.js
@@ -1,21 +1,50 @@
 import React, {useState} from 'react';
 
 function InputSample(){
-    const [text, setText] = useState('');
+    const [inputs, setInputs] = useState({
+        name:'',
+        nickname:'',
+    });
+    const {name, nickname} = inputs;
+
     const onChange = (e)=>{
-        setText(e.target.value);
+        // console.log(e.target.name);
+        // console.log(e.target.value);
+        const {name, value} = e.target; 
+        setInputs({
+            ...inputs,
+            [name]:value, //실제 name값이 무엇을 가르키고있는지에따라 다른 key값을 가르킨다.
+        });
     }
     const onReset = ()=>{
-        setText('');
+        setInputs({
+            name:'',
+            nickname:'',
+        })
     }
 
     return(
         <div>
-          <input onChange={onChange} value={text}/>
-          <button onClick={onReset}>초기화</button>
+          <input 
+            name="name" 
+            placeholder="이름" 
+            onChange={onChange} 
+            value={name}
+          />
+          <input 
+            name="nickname" 
+            placeholder="닉네임" 
+            onChange={onChange} 
+            value={nickname}
+          />
+          <button 
+            onClick={onReset}
+          >초기화
+          </button>
+          
           <div>
               <b>값 :</b>
-              {text}
+              {name} ({nickname})
           </div>
         </div>
     )
