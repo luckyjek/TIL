521fdf1d53c8e7e018ee88ea2b72d57d345d3c7d
luckyjek <70943889+luckyjek@users.noreply.github.com>
2022-03-06 12:59:40 +0900

    change classComponent

--- basic-react/src/components/hello.js
+++ basic-react/src/components/hello.js
@@ -1,14 +1,30 @@
-import React from 'react';
-function Hello({color,name, isSpecial}) {
-    //false값들은 다 안나타나는데 그중 0 예외.
-    return( 
-    <div style={{color:color}}>
-        {isSpecial && <b>*</b>}
-        안녕하세요 {name}< /div>
-    );
-}
+import React,{Component} from 'react';
 
-Hello.defaultProps = {
-    name:'이름없음'
-  }
+class Hello extends Component{
+    static defaultProps = {
+        name:'이름없음',
+    };    
+    
+        render(){
+            const {color, isSpecial, name} = this.props;
+            return (
+                <div style={{color}}>
+                    {isSpecial && <b>*</b>}
+                    안녕하세요{name}
+                </div>
+            )
+        }
+    }
+// function Hello({color,name, isSpecial}) {
+//     //false값들은 다 안나타나는데 그중 0 예외.
+//     return( 
+//     <div style={{color:color}}>
+//         {isSpecial && <b>*</b>}
+//         안녕하세요 {name}< /div>
+//     );
+// }
+
+// Hello.defaultProps = {
+//     name:'이름없음'
+//   }
 export default Hello;
