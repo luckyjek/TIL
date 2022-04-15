0ee9214442825135a24881592bcf3bc560066f49
luckyjek <thxjek@gmail.com>
2022-04-15 23:38:13 +0900

    Add 1.iterable.js

---
+++ basic-js/1.iterable.js
@@ -0,0 +1,10 @@
+const array = [1, 2, 3];
+console.log(array.values());
+console.log(array.enterise());
+console.log(array.keys());
+
+// iterator 사용해 보기!
+
+for(let item of array.valaue()){
+    console.log(item);
+}
