5612d5ca8f955403f5640526a89e6baa67d52242
luckyjek <thxjek@gmail.com>
2022-04-16 18:17:14 +0900

    Add 5.spread.js

---
+++ basic-js/4.generator.js
@@ -0,0 +1,33 @@
+// const multiple = {
+//     [Symbol.iterator]: () => {
+//         const max = 10;
+//         let num = 0;
+//         return {
+//             next() {
+//                 return {value: num++ *2, done: num > max}
+//             }
+//         }
+//     }
+// }
+
+function* multipleGenerator() {
+    try{
+        for(let i = 0; i < 10; i++) {
+            console.log(i);
+            yield i ** 2;
+        }
+    }catch(error) {
+        console.log(error);
+    }
+    
+}
+const multiple = multipleGenerator();
+let next = multiple.next();
+
+// multiple.return();
+multiple.throw('Error!');
+
+console.log(next.value, next.done);
+
+next = multiple.next();
+console.log(next.value, next.done);
