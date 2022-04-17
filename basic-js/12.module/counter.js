5b234da9c6346e5ea41747141e81df1d94fdd4d3
luckyjek <thxjek@gmail.com>
2022-04-17 22:25:48 +0900

    Add module.js (counter.js and main.js)

---
+++ basic-js/12.module/counter.js
@@ -0,0 +1,10 @@
+let count = 0;
+// export default function increase(){
+
+export function increase(){
+    count++;
+    console.log(count);
+}
+export function getCount(){
+    return count;
+}
---
+++ basic-js/12.module/main.js
@@ -0,0 +1,11 @@
+// import increase1 from './counter.js';
+
+//import {increase} from './counter.js';
+// import {increase as increase1} from './counter.js';
+//import {increase, getCount, } from './counter.js';
+import * as counter from './counter.js';
+
+counter.increase();
+counter.increase();
+counter.increase();
+console.log('getCount()', counter.getCount());
