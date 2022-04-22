00a0f95c3393c22e81dcfb39aa73cee9e7c3f2df
luckyjek <thxjek@gmail.com>
2022-04-22 21:19:17 +0900

    Fix bug onRemove function

--- supplement-react/todo-list/src/components/todoItem.jsx
+++ supplement-react/todo-list/src/components/todoItem.jsx
@@ -68,7 +68,7 @@ function TodoItem({id, done, text}){
             <CheckCircle done={done} onClick={onToggle}
             >{done && <MdDone />}</CheckCircle>
             <Text done={done}>{text}</Text>
-            <Remove onRemove={onRemove}>
+            <Remove onClick={onRemove}>
                 <MdDelete />
             </Remove>
          </TodoItemBlock>
