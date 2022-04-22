a7ee604e00a8c4a655ea9013b6ec1fdaba153e09
luckyjek <thxjek@gmail.com>
2022-04-22 19:13:08 +0900

    Add TodoCreate component

--- supplement-react/todo-list/src/App.js
+++ supplement-react/todo-list/src/App.js
@@ -3,6 +3,7 @@ import {createGlobalStyle} from 'styled-components';
 import TodoTemplate from './components/todoTemplate';
 import TodoHead from './components/todoHead';
 import TodoList from './components/todoList';
+import TodoCreate from './components/todoCreate';
 const GlobalStyle = createGlobalStyle`
   body{
     background: #e9ecef;
@@ -15,6 +16,7 @@ function App() {
       <TodoTemplate>
         <TodoHead />
         <TodoList />
+        <TodoCreate />
       </TodoTemplate>
     </>
   );
