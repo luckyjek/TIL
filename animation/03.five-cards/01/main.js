01018f2d0b6d95bf98d2b9f516409461be53c828
luckyjek <thxjek@gmail.com>
2022-04-24 19:15:38 +0900

    Add mousemove, mousedown and touchmove

--- animation/03.five-cards/01/main.js
+++ animation/03.five-cards/01/main.js
@@ -15,8 +15,24 @@ function clickHandler(e){
 }
 
 function moveHandler(e) {
-    console.log(e.clientX, e.clientY);
+    // console.log(e.clientX, e.clientY);
     handElem.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
 }
+
+function touchHandler(e) {
+    console.log(e.touches[0].clientX, e.touches[0].clientY);
+    handElem.style.transform = `translate(${e.touches[0].clientX}px, ${e.touches[0].clientY}px)`;
+}
+
 worldElem.addEventListener('click', clickHandler);
 window.addEventListener('mousemove', moveHandler);
+window.addEventListener('mousedown', () => {
+    document.body.classList.add('mousedown');
+});
+window.addEventListener('mouseup', () => {
+    document.body.classList.remove('mousedown');
+});
+
+//터치 기기에서는 어떻게 해야될까?
+//touchstart, touchend
+window.addEventListener('touchmove', touchHandler);
