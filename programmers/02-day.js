00d796d00e02ae903063653f86d907413ff3b152
luckyjek <thxjek@gmail.com>
2023-03-09 10:56:14 +0900

    숫자 비교하기

--- programmers/02-day.js
+++ programmers/02-day.js
@@ -1,8 +1,20 @@
 // Day 2 사칙연산, 조건문, 배열
+// function solution(num1, num2) {
+//   const reault = (num1 / num2) * 1000
+//   return Math.floor(reault);
+// }
+
+// console.log(solution(7, 3))
+
 function solution(num1, num2) {
-  const reault = (num1 / num2) * 1000
-  return Math.floor(reault);
+  let result = 0;
+  if (num1 === num2) {
+    result = 1;
+    return result;
+  }
+  else { 
+    result = -1;
+    return result;
+  }
 }
-
-console.log(solution(7, 3))
-
+console.log(solution(3,4))
