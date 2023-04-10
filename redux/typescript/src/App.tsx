6deebe6230d1b6f2647492f756f22124279cbb45
luckyjek <thxjek@gmail.com>
2023-04-10 19:13:17 +0900

    TypeScript로 리액트 상태관리하기

--- redux/typescript/src/App.tsx
+++ redux/typescript/src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import MyForm from './MyForm';
+import ReducerSample from './ReducerSample';
 
 
 const App: React.FC = () => {
-  const onSubmit = (form: { name: string; description: string }) => {
-    console.log(form);
-  };
-  return <MyForm onSubmit={onSubmit} />;
+  return <ReducerSample />;
 };
 
 export default App;
--- redux/typescript/src/Counter.tsx
+++ redux/typescript/src/Counter.tsx
@@ -1,14 +1,23 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+
+type Action = { type: 'INCREASE' } | { type: 'DECREASE' }; // 이렇게 액션을 | 으로 연달아서 쭉 나열하세요.
+
+function reducer(state: number, action: Action): number {
+  switch (action.type) {
+    case 'INCREASE':
+      return state + 1;
+    case 'DECREASE':
+      return state - 1;
+    default:
+      throw new Error('Unhandled action');
+  }
+}
 
 function Counter() {
+  const [count, dispatch] = useReducer(reducer, 0);
+  const onIncrease = () => dispatch({ type: 'INCREASE' });
+  const onDecrease = () => dispatch({ type: 'DECREASE' });
 
-  //  useState 를 사용 할 때 어떤 상황에 Generics 를 사용하는게 좋을까요?
-  // 바로, 상태가 null일 수도 있고 아닐수도 있을때 Generics 를 활용하시면 좋습니다.
-//   type Information = { name: string; description: string };
-// const [info, setInformation] = useState<Information | null>(null);
-  const [count, setCount] = useState<number>(0);
-  const onIncrease = () => setCount(count + 1);
-  const onDecrease = () => setCount(count - 1);
   return (
     <div>
       <h1>{count}</h1>
