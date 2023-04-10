f156a20af2ddf0ebc82e757416ff7d93bc45401d
luckyjek <thxjek@gmail.com>
2023-04-10 15:18:56 +0900

    vlpt 본격적 Redux 공부

--- redux/learn-redux/src/index.js
+++ redux/learn-redux/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './BasicRedux';
+import App from './App';
 import './exericise';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
