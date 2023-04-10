376475b38111060e7292e916830ad5e941404fb4
luckyjek <thxjek@gmail.com>
2023-04-10 18:39:58 +0900

    Type 적용한 MyForm

--- redux/typescript/src/App.tsx
+++ redux/typescript/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import Counter from './Counter';
+import MyForm from './MyForm';
 
-import './App.css';
 
-function App() {
-  return ( <Counter />
-  );
-}
+const App: React.FC = () => {
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
+  };
+  return <MyForm onSubmit={onSubmit} />;
+};
 
 export default App;
