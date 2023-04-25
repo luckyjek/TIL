279eb74e329a1c29435c3ceea6d32bee5ca5d987
luckyjek <thxjek@gmail.com>
2023-04-25 18:02:27 +0900

    Feat : Slice 패턴의 reducers 설계

---
+++ redux/todo/src/store/todo/index.ts
@@ -0,0 +1,72 @@
+import { PayloadAction, createSlice, nanoid } from '@reduxjs/toolkit';
+import { TodoState } from './type';
+import { useInjectReducer } from 'redux-injectors';
+
+export const initialState: TodoState = {
+  todolist: [],
+};
+
+const slice = createSlice({
+  name: 'todo',
+  initialState: initialState,
+  reducers: {
+    addTodo: {
+      reducer: (state, action: PayloadAction<ITodoItem>) => {
+        state.todolist.push(action.payload);
+      },
+      prepare: (content: string) => {
+        //todo를 추가할때, 컴포넌트에서는 todo의 내용만 구현? 해주기때문에 prepare에서
+        // 내용을 받고, ITodoItem Object로 만들어준후 reducer에서 상태저장해주기로하자.
+        const id = nanoid();
+        return {
+          payload: {
+            id: id,
+            content: content,
+            completed: false,
+            editing: false,
+          },
+        };
+      },
+      checkTodo(state, action: PayloadAction<{ id: string }>) {
+        const id = action.payload.id;
+        const todo = state.todolist.find(todo => todo.id === id);
+        if (todo) {
+          todo.completed = !todo.completed;
+        }
+      },
+      editModeTodo(state, action: PayloadAction<{ id: string }>) {
+        const id = action.payload.id;
+
+        for (const todo of state.todolist) {
+          if (todo.id === id) continue;
+          if (todo.editing === true) todo.editing = false;
+        }
+
+        const todo = state.todolist.find(todo => todo.id === id);
+        if (todo) {
+          todo.editing = !todo.editing;
+        }
+      },
+      editTodo(state, action: PayloadAction<{ id: string; content: string }>) {
+        const id = action.payload.id;
+        const content = action.payload.content;
+
+        const todo = state.todolist.find(todo => todo.id === id);
+        if (todo) {
+          todo.content = content;
+        }
+      },
+      deleteTodo(state, action: PayloadAction<{ id: string }>) {
+        const id = action.payload.id;
+        const filteredTodos = state.todolist.filter(todo => todo.id !== id);
+        state.todolist = filteredTodos;
+      },
+    },
+  },
+});
+
+export const { actions: TodoActions } = slice;
+export const useTodoSlice = () => {
+  useInjectReducer({ key: slice.name, reducer: slice.reducer });
+  return { TodoActions: slice.actions };
+};
--- redux/todo/src/types/RootState.ts
+++ redux/todo/src/types/RootState.ts
@@ -1,9 +1,12 @@
 // [IMPORT NEW CONTAINERSTATE ABOVE] < Needed for generating containers seamlessly
 
+import { TodoState } from 'store/todo/type';
+
 /* 
   Because the redux-injectors injects your reducers asynchronously somewhere in your code
   You have to declare them here manually
 */
 export interface RootState {
+  todo: TodoState;
   // [INSERT NEW REDUCER KEY ABOVE] < Needed for generating containers seamlessly
 }
